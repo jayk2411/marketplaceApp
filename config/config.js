07128da8806dbcdeb2421b879e781bfb7e91b47e
jaykumar kakadiya <jaykakdiya2411@gmail.com>
2024-02-16 15:27:40 -0500

    removed link of mongo

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
     env: process.env.NODE_ENV || 'development', 
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
-    mongoUri: process.env.MONGODB_URI || "mongodb+srv://jaykakdiya2411:Jaykakadiya2411@marketapp.n7ayjcj.mongodb.net/MarketPlace?retryWrites=true&w=majority&appName=mongosh+2.1.1"||
+    mongoUri: process.env.MONGODB_URI || ""||
     process.env.MONGO_HOST ||
     'mongodb://' + (process.env.IP || 'localhost') + ':' + 
    (process.env.MONGO_PORT || '27017') +
